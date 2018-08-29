a5f4a1c1551344347547ba721527d75e8677c96e
Yongcheol Cho <redvenus21@gmail.com>
2018-08-29 08:33:54 +0000

    add faker

--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 from flask import Flask, render_template, request
 import random
 import csv
+from faker import Faker
 
 names = []
 
 app = Flask(__name__)
+fake = Faker('ko-KR')
 
 @app.route("/")
 def index():
@@ -34,9 +36,17 @@ def result():
 @app.route("/admin")
 def admin():
     # names 에 들어가 있는 모든 이름을 출력
-    f= open('name.csv','r')
+    f= open('names.csv','r')
     rr = csv.reader(f)
     names = rr
     return render_template('admin.html', names)
     
+@app.route("/ffaker")
+def ffaker():
+    name = fake.name()
+    address = fake.address()
+    job = fake.job()
+    return render_template('ffaker.html', name = name, address=address, job=job)
+
+
 #app.run(host='0.0.0.0', port='8080', debug=True)
